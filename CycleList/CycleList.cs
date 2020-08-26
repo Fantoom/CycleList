765c27127a6f2ac047de5599941473c808f2c661
Fantoom <serge.vardanyan00@mail.ru>
2020-08-26 18:19:53 +0400

    init

---
+++ CycleList/CycleList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace System.Collections.Generic
+{
+	public class CycleList<T> : List<T> , IList<T>
+	{
+		private int currentIndex = -1;
+		public int CurrentIndex 
+		{
+			get => currentIndex; 
+			set 
+			{
+				if (value > 0)
+					currentIndex = GetIndex(value);
+				else Reset(); 
+			}
+		}
+		/// <summary>
+		///	Returns element by index and sets currentIndex to index
+		/// </summary>
+		public new T this[int index] 
+		{
+			get 
+			{
+				currentIndex = index;
+				return base[GetIndex(index)]; 
+			} 
+			set 
+			{
+				base[GetIndex(index)] = value;
+			}
+		}
+		/// <summary>
+		///	Returns next element
+		/// </summary>
+		public T GetNext()
+		{
+			currentIndex = GetIndex(currentIndex + 1);
+			return this[currentIndex];
+		}
+
+		/// <summary>
+		///	Sets currentIndex to -1
+		/// </summary>
+		public void Reset()
+		{
+			currentIndex = -1;
+		}
+
+		private int GetIndex(int index)
+		{
+			return index - index / this.Count * this.Count;
+		}
+	}
+}
---
+++ CycleListTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CycleListTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cycleList = new CycleList<int>() { 0, 1, 2, 3, 4 };
+			for (int i = 0; i < 15; i++)
+			{
+				Console.Write(cycleList[i] + " ");
+			}
+			Console.WriteLine();
+			cycleList.Reset();
+			for (int i = 0; i < 15; i++)
+			{
+				Console.Write(cycleList.GetNext() + " ");
+			}
+		}
+	}
+}
